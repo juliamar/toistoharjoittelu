f9bf27071e982d82fc71624924b3cd18c7c2173d
juliamar <juliam@live.fi>
2014-01-15 17:29:09 +0200

    Harjoitustyö lisätty

---
+++ Toistoharjoittelu/src/main/java/toistoharjoittelu/toistoharjoittelu/App.java
@@ -0,0 +1,13 @@
+package toistoharjoittelu.toistoharjoittelu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
